5dcd5b8c8a35238e19292ec95b84abea0e03ce71
JoshHerzuk <98534840+JoshHerzuk@users.noreply.github.com>
2022-04-20 17:28:22 -0400

    added pokeball icon

---
+++ script.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import ttk
+
+
+
+def main():
+
+
+    root = Tk()
+    root.title("Pokemon Info Viewer")
+    root.iconbitmap('pokeball.ico')
+
+
+
+
+
+
+    root.mainloop()   
+
+
+
+
+
+main()
